9a4409bce89ac2f617f44aeb6615151d0dbc5d2e
GermoLaz <57226070+GermoLaz@users.noreply.github.com>
2020-06-01 17:57:48 -0300

    class Input and Loader

---
+++ src/ar/edu/utn/mdp/Loader.java
@@ -0,0 +1,14 @@
+package ar.edu.utn.mdp;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class Loader{
+    private ArrayList<BufferedImage> sprites;
+
+    public Loader() {
+        this.sprites = new ArrayList<>();
+    }
+
+    private BufferedImage loadImage(){}
+}
