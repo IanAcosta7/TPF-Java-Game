88fdffe85172162b53793195074491e3fd032ec8
geronimoyatel <56684695+geronimoyatel@users.noreply.github.com>
2020-06-02 23:06:22 -0300

    cambios Clase Player

--- src/Player.java
+++ src/Player.java
@@ -5,13 +5,15 @@ public class Player {
     private int speed;
     private int fuel;
 
+    //************************constructor****************************************
 
     public Player(int speed, int fuel) {
         this.speed = speed;
         this.fuel = fuel;
     }
 
-    //***********************************************************************
+    //**************************GetsAndSets**************************************
+
     public int getSpeed() {
         return speed;
     }
@@ -27,5 +29,17 @@ public class Player {
     public void setFuel(int fuel) {
         this.fuel = fuel;
     }
-    //***********************************************************************
+
+    //***************************Metods******************************************
+
+    public void editSpeed(){
+
+
+    }
+
+    public void editFuel(){
+
+    }
+
+
 }
